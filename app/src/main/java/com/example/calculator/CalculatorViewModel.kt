363ed9c7247052db437facf71ff394767aeb1cca
genericen <shadmanbadhon1@gmail.com>
2026-02-18 09:08:17 +0600

    Implement CalculatorViewModel for calculator logic

---
+++ app/src/main/java/com/example/calculator/CalculatorViewModel.kt
@@ -0,0 +1,103 @@
+package com.example.calculator
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+
+class CalculatorViewModel : ViewModel() {
+
+    var state by mutableStateOf(CalculatorState())
+        private set
+
+    fun onAction(action: CalculatorAction) {
+        when(action) {
+            is CalculatorAction.Number -> enterNumber(action.number)
+            is CalculatorAction.Decimal -> enterDecimal()
+            is CalculatorAction.Clear -> state = CalculatorState()
+            is CalculatorAction.Operation -> enterOperation(action.operation)
+            is CalculatorAction.Calculate -> performCalculation()
+            is CalculatorAction.Delete -> performDeletion()
+        }
+    }
+
+    private fun enterOperation(operation: CalculatorOperation) {
+        if(state.number1.isNotBlank()) {
+            state = state.copy(operation = operation)
+        }
+    }
+
+    private fun performDeletion() {
+        when {
+            state.number2.isNotBlank() -> {
+                state = state.copy(
+                    number2 = state.number2.dropLast(1)
+                )
+            }
+            state.operation != null -> {
+                state = state.copy(
+                    operation = null
+                )
+            }
+            state.number1.isNotBlank() -> {
+                state = state.copy(
+                    number1 = state.number1.dropLast(1)
+                )
+            }
+        }
+    }
+
+    private fun performCalculation() {
+        val number1 = state.number1.toDoubleOrNull()
+        val number2 = state.number2.toDoubleOrNull()
+        if(number1 != null && number2 != null && state.operation != null) {
+            val result = when(state.operation) {
+                is CalculatorOperation.Add -> number1 + number2
+                is CalculatorOperation.Subtract -> number1 - number2
+                is CalculatorOperation.Multiply -> number1 * number2
+                is CalculatorOperation.Divide -> number1 / number2
+                null -> return
+            }
+            // Format to remove unnecessary decimal .0
+            val formattedResult = if (result % 1.0 == 0.0) {
+                result.toLong().toString()
+            } else {
+                result.toString()
+            }
+            
+            state = CalculatorState(
+                number1 = formattedResult,
+                number2 = "",
+                operation = null
+            )
+        }
+    }
+
+    private fun enterDecimal() {
+        if(state.operation == null && !state.number1.contains(".") && state.number1.isNotBlank()) {
+            state = state.copy(
+                number1 = state.number1 + "."
+            )
+            return
+        }
+        if(!state.number2.contains(".") && state.number2.isNotBlank()) {
+            state = state.copy(
+                number2 = state.number2 + "."
+            )
+        }
+    }
+
+    private fun enterNumber(number: Int) {
+        if(state.operation == null) {
+            if(state.number1.length >= 8) return // Limit length
+            state = state.copy(
+                number1 = state.number1 + number
+            )
+            return
+        }
+        if(state.number2.length >= 8) return
+        state = state.copy(
+            number2 = state.number2 + number
+        )
+    }
+}
