ba928a58a78ca9ee223ac93104129ca6fbf241b1
genericen <shadmanbadhon1@gmail.com>
2026-02-18 09:06:14 +0600

    Add CalculatorState and action classes

---
+++ app/src/main/java/com/example/calculator/CalculatorState.kt
@@ -0,0 +1,23 @@
+package com.example.calculator
+
+sealed class CalculatorAction {
+    data class Number(val number: Int) : CalculatorAction()
+    object Clear : CalculatorAction()
+    object Delete : CalculatorAction()
+    object Decimal : CalculatorAction()
+    object Calculate : CalculatorAction()
+    data class Operation(val operation: CalculatorOperation) : CalculatorAction()
+}
+
+sealed class CalculatorOperation(val symbol: String) {
+    object Add : CalculatorOperation("+")
+    object Subtract : CalculatorOperation("-")
+    object Multiply : CalculatorOperation("×")
+    object Divide : CalculatorOperation("÷")
+}
+
+data class CalculatorState(
+    val number1: String = "",
+    val number2: String = "",
+    val operation: CalculatorOperation? = null
+)
