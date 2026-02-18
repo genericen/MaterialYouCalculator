2458446a30db3177a622fa0f08032f5d4a000ffe
genericen <shadmanbadhon1@gmail.com>
2026-02-18 13:25:54 +0600

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,5 +1,6 @@
-// Top-level build file
 plugins {
-    id("com.android.application") version "8.2.2" apply false
+    // Update this version from 8.2.2 to 8.6.0
+    id("com.android.application") version "8.6.0" apply false
+    id("com.android.library") version "8.6.0" apply false
     id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 }
